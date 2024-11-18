f26feaddf63767c9f21d0fc05a0d216a38407482
Daffa Randika <daffa.randika.3@gmail.com>
2024-11-18 19:51:22 +0700

    fix: trapeziod functions are now more accurate

--- integration_calculator/calculator/views/trapesium_view.py
+++ integration_calculator/calculator/views/trapesium_view.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+import math
 from plotly.io import to_html
-import numpy as np
 from numpy import cos
 from numpy import e
 from numpy import linspace
@@ -15,7 +15,7 @@ from sympy import symbols, sympify, lambdify
 from sympy.utilities.autowrap import ufuncify
 
 class TrapesiumView(View):
-    context = {"result": "", "a": "", "b": "", "fx": "", "n": "", "delta_x": "", "fig": ""}
+    context = {"error": "", "hasil": "", "a": "", "b": "", "fx": "", "n": "", "delta_x": "", "fig": ""}
     template_file = 'trapesium.html'
 
     def get(self, request):
@@ -51,33 +51,48 @@ class TrapesiumView(View):
                 self.integral_trapesium_delta_x(eval(a), eval(b), delta_x, fx_sym)
 
             else:
-                self.context["result"] = "Invalid input"
+                self.context["hasil"] = "Invalid input"
         
         except Exception as err:
-            self.context["result"] = f"Error: {str(err)}"
+            self.context["hasil"] = f"Error: {str(err)}"
 
         return render(request, 'trapesium.html', self.context)
 
 
     def integral_trapesium_n(self, a, b, n, fx, graph=True):
+        # menghitung nilai integral numerik
         def f(x):
             return fx.subs(symbols("x"), x)
         
         delta_x = (b - a)/(n)
-        x_hitung = linspace(a,b,n)
+        print(f"{delta_x=}")
+        x_hitung = linspace(a,b,n+1)
         
         jumlah =  f(a) + f(b)
-        for i in range(1, n-1):
+        for i in range(1, n):
+            print(f"loopp ==== {i=}")
+            print(f"loopp ==== {x_hitung[i]=}")
+            print(f"loopp ==== {f(x_hitung[i])=}")
             jumlah += 2 * f(x_hitung[i])
+
+        print(jumlah)
 
         hasil = (delta_x/2) * jumlah
         print(hasil)
-        self.context["result"] = hasil
+        self.context["hasil"] = hasil
 
+        # menghitung nilai error
+        int_real = self.calc_real_integral(a, b, fx)
+        error = self.calc_error(int_real, hasil)
+        self.context["error"] = error
+
+        # menggambar graf
         if graph:
             y_hitung = lambdify("x", fx, modules=["numpy"])(x_hitung)
-            x_real = linspace(a-1,b+1,100)
+            x_real = linspace(a,b,100)
+            x_real_padding = linspace(a-1,b+1,100)
             y_real = lambdify("x", fx, modules=["numpy"])(x_real)
+            y_real_padding = lambdify("x", fx, modules=["numpy"])(x_real_padding)
             print(y_real)
             df = pd.DataFrame({'x': x_real, 'y': y_real})
             fig = px.line(df, x='x', y='y')
@@ -93,6 +108,16 @@ class TrapesiumView(View):
                 ),
             )
 
+            fig.add_trace(
+                go.Line(
+                    x=x_real_padding,
+                    y=y_real_padding,
+                    showlegend=False,
+                    name="nilai asli",
+                    line=dict(color="deepskyblue"),
+                )
+            )
+
             for i in range(0, len(x_hitung)):
                 fig.add_trace(go.Scatter(
                     x=[x_hitung[i], x_hitung[i]],
@@ -106,28 +131,102 @@ class TrapesiumView(View):
                 go.Line(
                     x=x_real,
                     y=y_real,
-                    fill="tozeroy",
                     name="nilai asli",
                     line=dict(color="deepskyblue"),
+                    fill="tozeroy"
                 )
             )
+
 
             self.context["fig"] = to_html(fig, full_html=False, default_width="50%", 
                         include_plotlyjs="cdn", div_id="ohlc")
 
 
 
-    def integral_trapesium_delta_x(self, a, b, delta_x, fx):
+    def integral_trapesium_delta_x(self, a, b, delta_x, fx, graph=True):
+        # menghitung nilai integral numerik
         def f(x):
             return fx.subs(symbols("x"), x)
         
-        n = int((b - a)/(delta_x))
-        x = linspace(a,b,n)
+        n = math.ceil((b - a)/(delta_x))+1
+        x_hitung = linspace(a,b,n)
         
         jumlah =  f(a) + f(b)
         for i in range(1, n-1):
-            jumlah += 2 * f(x[i])
+            jumlah += 2 * f(x_hitung[i])
+
+        print(jumlah)
 
         hasil = (delta_x/2) * jumlah
 
-        return hasil
+        self.context["hasil"] = hasil
+
+        # menghitung nilai error
+        int_real = self.calc_real_integral(a, b, fx)
+        print(f"{ int_real= }")
+        error = self.calc_error(int_real, hasil)
+        self.context["error"] = error
+
+        # menggambar graf
+        if graph:
+            y_hitung = lambdify("x", fx, modules=["numpy"])(x_hitung)
+            x_real = linspace(a,b,100)
+            x_real_padding = linspace(a-1,b+1,100)
+            y_real = lambdify("x", fx, modules=["numpy"])(x_real)
+            y_real_padding = lambdify("x", fx, modules=["numpy"])(x_real_padding)
+            print(y_real)
+            df = pd.DataFrame({'x': x_real, 'y': y_real})
+            fig = px.line(df, x='x', y='y')
+            fig = go.Figure()
+
+            fig.add_trace(
+                go.Scatter(
+                    x=x_hitung,
+                    y=y_hitung,
+                    fill="tozeroy",
+                    name="nilai hitung",
+                    line=dict(color='red')
+                ),
+            )
+
+            fig.add_trace(
+                go.Line(
+                    x=x_real_padding,
+                    y=y_real_padding,
+                    showlegend=False,
+                    name="nilai asli",
+                    line=dict(color="deepskyblue"),
+                )
+            )
+
+            for i in range(0, len(x_hitung)):
+                fig.add_trace(go.Scatter(
+                    x=[x_hitung[i], x_hitung[i]],
+                    y=[0, y_hitung[i]],
+                    mode='lines',
+                    line=dict(color='red'),
+                    showlegend=False
+                ))
+
+            fig.add_trace(
+                go.Line(
+                    x=x_real,
+                    y=y_real,
+                    name="nilai asli",
+                    line=dict(color="deepskyblue"),
+                    fill="tozeroy"
+                )
+            )
+
+
+            self.context["fig"] = to_html(fig, full_html=False, default_width="50%", 
+                        include_plotlyjs="cdn", div_id="ohlc")
+
+
+    def calc_real_integral(self, a,b,fx):
+        def f(x):
+            return fx.subs(symbols("x"), x)
+        return f(b) - f(a)
+
+    def calc_error(self, real, calculated):
+        return f"{((real-calculated)/real) * 100}%"
