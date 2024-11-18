0b1c03d20af95a9bca4bb8358d4fe91c85a0ec9f
Daffa Randika <daffa.randika.3@gmail.com>
2024-11-18 17:48:50 +0700

    feat: graph on trapeziod function

--- integration_calculator/calculator/views/trapesium_view.py
+++ integration_calculator/calculator/views/trapesium_view.py
@@ -1,19 +1,24 @@
 from django.views import View
 from django.shortcuts import render
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.io import to_html
+import numpy as np
 from numpy import cos
 from numpy import e
 from numpy import linspace
 from numpy import pi
 from numpy import sin
 from numpy import tan
-from sympy import symbols, sympify
+from sympy import symbols, sympify, lambdify
+from sympy.utilities.autowrap import ufuncify
 
 class TrapesiumView(View):
-    context = {"result": "", "a": "", "b": "", "fx": "", "n": "", "delta_x": ""}
+    context = {"result": "", "a": "", "b": "", "fx": "", "n": "", "delta_x": "", "fig": ""}
     template_file = 'trapesium.html'
 
     def get(self, request):
-        print(self.context)
         return render(request, self.template_file, self.context)
 
     def post(self, request):
@@ -32,8 +37,7 @@ class TrapesiumView(View):
                 f = sympify(fx)
                 fx_sym = f.subs(e_symbol, e).subs(pi_symbol, pi)
                 
-                result = self.integral_trapesium_n(eval(a), eval(b), n, fx_sym)
-                self.context["result"] = result
+                self.integral_trapesium_n(eval(a), eval(b), n, fx_sym)
 
             elif request.POST.get("delta_x") and not request.POST.get("n"):
                 delta_x = eval(request.POST.get("delta_x"))
@@ -44,8 +48,7 @@ class TrapesiumView(View):
                 f = sympify(fx)
                 fx_sym = f.subs(e_symbol, e).subs(pi_symbol, pi)
                 
-                result = self.integral_trapesium_delta_x(eval(a), eval(b), delta_x, fx_sym)
-                self.context["result"] = result
+                self.integral_trapesium_delta_x(eval(a), eval(b), delta_x, fx_sym)
 
             else:
                 self.context["result"] = "Invalid input"
@@ -53,24 +56,65 @@ class TrapesiumView(View):
         except Exception as err:
             self.context["result"] = f"Error: {str(err)}"
 
-        print(self.context)
         return render(request, 'trapesium.html', self.context)
 
 
-    def integral_trapesium_n(self, a, b, n, fx):
+    def integral_trapesium_n(self, a, b, n, fx, graph=True):
         def f(x):
             return fx.subs(symbols("x"), x)
         
         delta_x = (b - a)/(n)
-        x = linspace(a,b,n)
+        x_hitung = linspace(a,b,n)
         
         jumlah =  f(a) + f(b)
         for i in range(1, n-1):
-            jumlah += 2 * f(x[i])
+            jumlah += 2 * f(x_hitung[i])
 
         hasil = (delta_x/2) * jumlah
+        print(hasil)
+        self.context["result"] = hasil
 
-        return hasil
+        if graph:
+            y_hitung = lambdify("x", fx, modules=["numpy"])(x_hitung)
+            x_real = linspace(a-1,b+1,100)
+            y_real = lambdify("x", fx, modules=["numpy"])(x_real)
+            print(y_real)
+            df = pd.DataFrame({'x': x_real, 'y': y_real})
+            fig = px.line(df, x='x', y='y')
+            fig = go.Figure()
+
+            fig.add_trace(
+                go.Scatter(
+                    x=x_hitung,
+                    y=y_hitung,
+                    fill="tozeroy",
+                    name="nilai hitung",
+                    line=dict(color='red')
+                ),
+            )
+
+            for i in range(0, len(x_hitung)):
+                fig.add_trace(go.Scatter(
+                    x=[x_hitung[i], x_hitung[i]],
+                    y=[0, y_hitung[i]],
+                    mode='lines',
+                    line=dict(color='red'),
+                    showlegend=False
+                ))
+
+            fig.add_trace(
+                go.Line(
+                    x=x_real,
+                    y=y_real,
+                    fill="tozeroy",
+                    name="nilai asli",
+                    line=dict(color="deepskyblue"),
+                )
+            )
+
+            self.context["fig"] = to_html(fig, full_html=False, default_width="50%", 
+                        include_plotlyjs="cdn", div_id="ohlc")
+
 
 
     def integral_trapesium_delta_x(self, a, b, delta_x, fx):
